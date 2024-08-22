4aabfab4e6244a2e5ae3631bd64723d52a11beb4
DaviEspinosa <davipedrosa037@gmail.com>
2024-08-22 17:05:06 -0300

    ok

--- GerenciamentoConsultasMedicas/resources/views/parts/header.blade.php
+++ GerenciamentoConsultasMedicas/resources/views/parts/header.blade.php
@@ -5,10 +5,13 @@
                 <li><a href="">Meus Agendamentos</a></li>
                 <li><a href="">Dúvidas</a></li>
             </div>
-        
-        <div class="cadastro_login">
-            <li><a href="/login">Entrar</a></li>
-            <li><a href="/cadastro">Cadastrar</a></li>
-        </div>
+            <div class="cadastro_login">
+                <select class="login-select" onchange="handleSelectChange(this)">
+                    <option value="">Entrar</option>
+                    <option value="/login/paciente">Entrar como Paciente</option>
+                    <option value="/login/medico">Entrar como Médico</option>
+                </select>
+                <a href="/cadastro">Cadastrar</a>
+            </div>
     </div>
 </header>
